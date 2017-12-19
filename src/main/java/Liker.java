5dedae13c7a5b10e996cc4dd839194e04fcecf34
Arthur Barsegyan <a.barsegyan96@gmail.com>
2017-12-19 23:13:25 +0700

    Write code for setting liker instance by user

--- src/main/java/Liker.java
+++ src/main/java/Liker.java
@@ -2,8 +2,24 @@ import org.jinstagram.Instagram;
 
 public class Liker {
     private Instagram instagram;
+    private String targetHashTag;
+
+    private Integer likeThreshold;
+    private Integer likesCount;
 
     Liker(Instagram instagram) {
         this.instagram = instagram;
+    }
+
+    public void setTargetHashTag(String targetHashTag) {
+        this.targetHashTag = targetHashTag;
+    }
+
+    public void setLikeThreshold(Integer likeThreshold) {
+        this.likeThreshold = likeThreshold;
+    }
+
+    public void setLikesCount(Integer likesCount) {
+        this.likesCount = likesCount;
     }
 }
