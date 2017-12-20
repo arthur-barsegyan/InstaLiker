3c8d0bd4115e1dd7aa53b30dbadb67bd96569e59
Arthur Barsegyan <a.barsegyan96@gmail.com>
2017-12-20 20:36:43 +0700

    Implement command for getting active list of likers

--- src/main/java/ru/nsu/instaliker/view/ConsoleView.java
+++ src/main/java/ru/nsu/instaliker/view/ConsoleView.java
@@ -1,7 +1,10 @@
 package ru.nsu.instaliker.view;
 
+import ru.nsu.instaliker.Liker;
 import ru.nsu.instaliker.Manager;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class ConsoleView extends Thread {
@@ -10,7 +13,8 @@ public class ConsoleView extends Thread {
     private Manager manager;
 
     public String readString() { return reader.nextLine(); }
-    public void printString(String text) { System.out.println(text); }
+    public void printlnString(String text) { System.out.println(text); }
+    public void printString(String text) { System.out.print(text); }
 
     public void setManager(Manager manager) {
         this.manager = manager;
@@ -20,11 +24,16 @@ public class ConsoleView extends Thread {
     public synchronized void start() {
         boolean isEnd = false;
         while (!isEnd) {
+            System.out.print("Enter command > ");
             String command = readString();
             switch (command) {
                 case "list":
+                    ArrayList<Liker> likers = manager.getLikersList();
+                    for (Iterator<Liker> it = likers.iterator(); it.hasNext(); )
+                        System.out.println("\t" + it.next());
+                    break;
 
-                    default:
+                default:
 
             }
         }
